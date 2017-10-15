62e75c4e1f6b1012fac310ea35993cd78e04e374
Luiz Carneiro <luuizpaulo@gmail.com>
2017-10-15 13:26:40 +0200

    Created github repository (readme.md added)

--- README.md
+++
@@ -1,2 +0,0 @@
-# VaniJS_ultimate_TikTakToe
-An unbeatable TikTakToe game written in vanilla JS
---
+++ js/script.js
@@ -0,0 +1,25 @@
+elements = document.getElementsByTagName('td');
+function fill() {
+  if (this.textContent === '') {
+    this.textContent = 'X';
+  } else if (this.textContent === 'X') {
+    this.textContent = 'O';
+  } else if (this.textContent === 'O') {
+    this.textContent = '';
+  }
+}
+//const fill = () => this.texContent= 'X';
+const restart = () => {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].textContent = '';
+  }
+};
+button = document.getElementsByClassName('btn');
+button[0].onclick = restart;
+
+for (i = 0; i < elements.length; i++) {
+  elements[i].addEventListener('click', fill, false);
+}
+
+//const test = event => console.log(event);
+//elements[0].addEventListener('click', test, false);
